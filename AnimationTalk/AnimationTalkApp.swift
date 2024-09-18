b04dacd3da481dcb0bd06ad4629ffbad05c7c964
sweetclimusic <4057640+sweetclimusic@users.noreply.github.com>
2024-09-18 13:40:39 +0100

    Initial Commit

---
+++ AnimationTalk/AnimationTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTalkApp.swift
+//  AnimationTalk
+//
+//  Created by Ashlee Muscroft on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationTalk
+//
+//  Created by Ashlee Muscroft on 18/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
